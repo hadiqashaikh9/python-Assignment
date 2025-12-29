813f6c5a61a6ee26ab2cd10057f43fcabae93e09
hadiqashaikh9 <hadiqashykh528@gmail.com>
2025-12-29 20:52:58 +0500

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,36 @@
+#arthematic operators
+a = 5
+b = 2
+print(a + b)    
+print(a - b)    
+print(a * b)    
+print(a / b)    
+print(a % b)    
+print(a ** b)   
+
+#relational operators
+a = 50
+b = 20
+print(a == b)   
+print(a != b)   
+print(a >= b)   
+print(a > b)    
+print(a <= b)   
+print(a < b)    
+
+#assignment operators
+a = 50
+b = 20
+print(a == b)   
+print(a != b)   
+print(a >= b)   
+print(a > b)    
+print(a <= b)   
+print(a < b)    
+
+#logical operators
+a = 10
+b = 5
+print(a > 5 and b < 10)
+print(a > 5 or b > 10)
+print(not(a > 5))
