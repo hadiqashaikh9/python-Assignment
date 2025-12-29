3790b9d6d7f0c2e49b5bb8968bbf41c200f2f164
hadiqashaikh9 <hadiqashykh528@gmail.com>
2025-12-29 20:19:24 +0500

    Add files via upload

---
+++ Expressions.py
@@ -0,0 +1,39 @@
+# Constant Expressions
+x = 15 + 1.3
+print(x)
+
+# Arithmetic Expressions
+x = 40
+y = 12
+
+add = x + y
+sub = x - y
+pro = x * y
+div = x / y
+
+print(add)
+print(sub)
+print(pro)
+print(div)
+# Integral Expressions
+a = 13
+b = 12.0
+
+c = a + int(b)
+print(c)
+#  Integral Expressions
+
+a = 10 + 3 * 4
+print(a)
+
+b = (10 + 3) * 4
+print(b)
+
+c = 10 + (3 * 4)
+print(c)
+# Floating Expressions
+a = 13
+b = 5
+
+c = a / b
+print(c)
